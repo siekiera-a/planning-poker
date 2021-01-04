9302d442b3014453469586e5c3e44ea7e3eac45f
siekiera-a <siekiera.a31@gmail.com>
2021-01-04 16:56:35 +0100

    database structure

---
+++ database/structure.sql
@@ -0,0 +1,100 @@
+create database planning_poker
+go
+
+use planning_poker
+go
+
+create table dbo.invitation
+(
+    id         int identity
+        constraint invitation_pk
+            primary key nonclustered,
+    meeting_id int not null,
+    user_id    int not null
+)
+go
+
+create table dbo.meeting
+(
+    id         int identity
+        constraint meeting_pk
+            primary key nonclustered,
+    start_time datetime2(0) not null,
+    end_time   datetime2(0),
+    team_id    int          not null
+)
+go
+
+create table dbo.refresh_token
+(
+    id              int identity
+        constraint refresh_token_pk
+            primary key nonclustered,
+    token           varchar(50)  not null,
+    expiration_time datetime2(0) not null,
+    user_id         int          not null
+)
+go
+
+create table dbo.result
+(
+    id             int identity
+        constraint result_pk
+            primary key nonclustered,
+    task_id        int      not null,
+    user_id        int      not null,
+    estimated_time smallint not null
+)
+go
+
+create table dbo.task
+(
+    id          int identity
+        constraint task_pk
+            primary key nonclustered,
+    description nvarchar(max) not null,
+    meeting_id  int           not null
+)
+go
+
+create table dbo.team
+(
+    id        int identity
+        constraint team_pk
+            primary key nonclustered,
+    name      nvarchar(100) not null,
+    join_code varchar(8),
+    owner_id  int           not null
+)
+go
+
+create unique index team_join_code_uindex
+    on dbo.team (join_code)
+go
+
+create table dbo.team_member
+(
+    id      int identity
+        constraint team_member_pk
+            primary key nonclustered,
+    team_id int not null,
+    user_id int not null
+)
+go
+
+create table dbo.[user]
+(
+    id       int identity
+        constraint user_pk
+            primary key nonclustered,
+    name     nvarchar(100) not null,
+    email    nvarchar(100) not null,
+    password varchar(100)  not null
+)
+go
+
+create unique index user_email_uindex
+    on dbo.[user] (email)
+go
+
+
