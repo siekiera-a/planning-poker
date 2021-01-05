d773ad94a263a5107a3aaab7d7431166e8ec6b43
siekiera-a <siekiera.a31@gmail.com>
2021-01-05 20:09:10 +0100

    CreateUser procedure

---
+++ database/procedures/user/CreateUser.sql
@@ -0,0 +1,18 @@
+CREATE PROCEDURE dbo.spUser_CreateUser @Name NVARCHAR(100),
+                                       @Email NVARCHAR(100),
+                                       @Password VARCHAR(100)
+AS
+BEGIN
+    SET NOCOUNT ON
+    DECLARE @Success BIT = 1
+
+    IF (SELECT COUNT(*) FROM [user] WHERE email = @Email) > 0
+        SET @Success = 0
+    ELSE
+        INSERT INTO [user] VALUES (@Name, @Email, @Password)
+
+    SELECT @Success
+END
+go
+
+
