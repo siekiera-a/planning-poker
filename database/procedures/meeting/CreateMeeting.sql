b27f17809e1b3b4876897335700607bbb6eab0d7
siekiera-a <siekiera.a31@gmail.com>
2021-01-05 23:55:50 +0100

    CreateMeeting procedure

---
+++ database/procedures/meeting/CreateMeeting.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE dbo.spMeeting_CreateMeeting @StartTime DATETIME2(0),
+                                             @TeamId INT,
+                                             @Organizer INT
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    INSERT INTO meeting VALUES (@StartTime, NULL, @TeamId, @Organizer)
+END
+go
+
+
