dbd62e305d64b464a0372ddf28c37e9c2315fe02
siekiera-a <siekiera.a31@gmail.com>
2021-01-06 00:37:08 +0100

    changed to utc time

--- database/procedures/refresh_token/RemoveExpiredTokens.sql
+++ database/procedures/refresh_token/RemoveExpiredTokens.sql
@@ -1,7 +1,7 @@
 CREATE PROCEDURE dbo.spRefreshToken_RemoveExpiredTokens
 AS
 BEGIN
-    DELETE FROM refresh_token WHERE expiration_time <= SYSDATETIME()
+    DELETE FROM refresh_token WHERE expiration_time <= SYSUTCDATETIME()
 END
 go
 
