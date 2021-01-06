678a17b45d76b9de37c47752de9fc486b3da1e57
siekiera-a <siekiera.a31@gmail.com>
2021-01-06 22:45:41 +0100

    JoinWithCode procedure

---
+++ database/procedures/team_member/JoinWithCode.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE dbo.spTeamMember_JoinWithCode @UserId INT,
+                                               @Code VARCHAR(8)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    DECLARE @TeamId INT
+
+    SELECT @TeamId = id FROM team WHERE join_code = @Code
+
+    IF @TeamId IS NOT NULL
+        EXEC dbo.spTeamMember_AddMember @TeamId, @UserId
+END
+go
+
+
