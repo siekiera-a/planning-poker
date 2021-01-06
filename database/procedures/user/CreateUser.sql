99fea6e3e42696c18cc9d3e85ccb3ff71a09a0a1
siekiera-a <siekiera.a31@gmail.com>
2021-01-06 01:31:05 +0100

    return user_id after creation

--- database/procedures/user/CreateUser.sql
+++ database/procedures/user/CreateUser.sql
@@ -4,14 +4,10 @@ CREATE PROCEDURE dbo.spUser_CreateUser @Name NVARCHAR(100),
 AS
 BEGIN
     SET NOCOUNT ON
-    DECLARE @Success BIT = 1
 
-    IF (SELECT COUNT(*) FROM [user] WHERE email = @Email) > 0
-        SET @Success = 0
-    ELSE
-        INSERT INTO [user] VALUES (@Name, @Email, @Password)
+    IF (SELECT COUNT(*) FROM [user] WHERE email = @Email) = 0
+        INSERT INTO [user] OUTPUT inserted.id VALUES (@Name, @Email, @Password)
 
-    SELECT @Success
 END
 go
 
