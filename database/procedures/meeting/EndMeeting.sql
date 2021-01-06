c3b890ace083b3a5766ffaf31bc59f00b8e3f2d9
siekiera-a <siekiera.a31@gmail.com>
2021-01-06 01:07:55 +0100

    EndMeeting procedure

---
+++ database/procedures/meeting/EndMeeting.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE dbo.spMeeting_EndMeeting @Id INT
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    DECLARE
+        @Time DATETIME2(0) = SYSUTCDATETIME(),
+        @Start DATETIME2(0),
+        @End DATETIME2(0)
+
+    SELECT @Start = start_time, @End = end_time FROM meeting WHERE id = @Id
+
+    IF @Start < @Time AND @End IS NULL
+        UPDATE meeting SET end_time = @Time WHERE id = @Id
+
+    SELECT id, start_time, end_time, team_id, organizer FROM meeting WHERE id = @Id
+END
+go
+
+
