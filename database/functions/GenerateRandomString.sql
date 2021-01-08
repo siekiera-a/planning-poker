2ca202f11c8c2f3973de14fc0f8c581bc2911479
siekiera-a <siekiera.a31@gmail.com>
2021-01-08 21:07:12 +0100

    GenerateRandomString function

---
+++ database/functions/GenerateRandomString.sql
@@ -0,0 +1,24 @@
+CREATE FUNCTION dbo.ufnGenerateRandomString(@Length INT) RETURNS VARCHAR(max)
+AS
+BEGIN
+    DECLARE
+        @String VARCHAR(max),
+        @i INT = 0,
+        @AvailableChars VARCHAR(max) = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+
+    DECLARE
+        @CharsCount INT = LEN(@AvailableChars)
+
+    WHILE @i < @Length
+        BEGIN
+            SET @String = CONCAT(@String,
+                                 SUBSTRING(@AvailableChars, ABS(CHECKSUM((SELECT new_id FROM NewId))) % @CharsCount + 1,
+                                           1))
+            SET @i = @i + 1
+        END
+
+    RETURN @String
+END
+go
+
+
