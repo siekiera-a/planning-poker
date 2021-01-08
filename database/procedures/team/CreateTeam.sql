696fcd3fc868bb044eaed873003e3b7a8c89bfec
siekiera-a <siekiera.a31@gmail.com>
2021-01-08 12:17:16 +0100

    CreateTeam, RemoveJoinCode procedures

---
+++ database/procedures/team/CreateTeam.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE dbo.spTeam_CreateTeam @Name NVARCHAR(100)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    INSERT INTO team OUTPUT inserted.id VALUES (@Name, NULL)
+END
+go
+
+
---
+++ database/procedures/team/RemoveJoinCode.sql
@@ -0,0 +1,10 @@
+CREATE PROCEDURE dbo.spTeam_RemoveJoinCode @Id INT
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    UPDATE team SET join_code = NULL WHERE id = @Id
+END
+go
+
+
