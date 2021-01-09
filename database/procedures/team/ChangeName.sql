044bf3eb76999c1ce07d691b3216b55cec54392d
siekiera-a <siekiera.a31@gmail.com>
2021-01-09 01:50:43 +0100

    GenerateJoinCode, ChangeName procedures

---
+++ database/procedures/team/ChangeName.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE dbo.spTeam_ChangeName @Id INT,
+                                       @NewName NVARCHAR(100)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    UPDATE team SET name = @NewName WHERE id = @Id
+END
+go
+
+
