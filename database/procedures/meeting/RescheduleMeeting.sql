413e678e3d32b53564106720dcc553076e0c08b4
siekiera-a <siekiera.a31@gmail.com>
2021-01-09 02:50:06 +0100

    RescheduleMeeting procedure

---
+++ database/procedures/meeting/RescheduleMeeting.sql
@@ -0,0 +1,14 @@
+CREATE PROCEDURE dbo.spMeeting_RescheduleMeeting @Id INT,
+                                                 @NewStartTime DATETIME2(0)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    -- meeting cannot be reschedule to the past time
+    IF @NewStartTime > SYSUTCDATETIME()
+        UPDATE meeting SET start_time = @NewStartTime WHERE id = @Id
+
+END
+go
+
+
