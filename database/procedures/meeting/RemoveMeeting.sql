c7709e37a621d9247a054c7a587a71426e544cce
siekiera-a <siekiera.a31@gmail.com>
2021-01-09 02:44:25 +0100

    RemoveMeeting procedure

---
+++ database/procedures/meeting/RemoveMeeting.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE dbo.spMeeting_RemoveMeeting @Id INT
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    -- remove meeting only if not started
+    IF ((SELECT start_time FROM meeting WHERE id = @Id) > SYSUTCDATETIME())
+        BEGIN
+            DELETE FROM invitation WHERE meeting_id = @Id
+            DELETE FROM task WHERE meeting_id = @Id
+            DELETE FROM meeting WHERE id = @Id
+        END
+END
+go
+
+
