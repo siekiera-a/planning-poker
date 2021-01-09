9f5f910c0b9cd2c3ae6c727fbf359b5cad5f30e2
siekiera-a <siekiera.a31@gmail.com>
2021-01-09 18:54:17 +0100

    fixed procedure after database structure change

--- database/procedures/team_member/RemoveMember.sql
+++ database/procedures/team_member/RemoveMember.sql
@@ -8,13 +8,9 @@ BEGIN
     -- delete invitations for future meetings
     DELETE
     FROM invitation
-    WHERE id IN
-          (SELECT i.id
-           FROM invitation i
-                    INNER JOIN meeting m ON i.meeting_id = m.id
-           WHERE m.start_time >= SYSUTCDATETIME()
-             AND i.user_id = @UserId
-             AND m.team_id = @TeamId)
+    WHERE user_id = @UserId
+      AND meeting_id IN
+          (SELECT id FROM meeting WHERE start_time >= SYSUTCDATETIME() AND team_id = @TeamId)
 END
 go
 
