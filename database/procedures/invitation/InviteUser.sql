154ef3159a62ac6981b0af6305f1b083058da09e
siekiera-a <siekiera.a31@gmail.com>
2021-01-09 02:26:33 +0100

    InviteUser, RemoveInvitation procedures

---
+++ database/procedures/invitation/InviteUser.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE dbo.spInvitation_InviteUser @MeetingId INT,
+                                             @UserId INT
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    INSERT INTO invitation VALUES (@MeetingId, @UserId)
+END
+go
+
+
---
+++ database/procedures/invitation/RemoveInvitation.sql
@@ -0,0 +1,11 @@
+CREATE PROCEDURE dbo.spInvitation_RemoveInvitation @MeetingId INT,
+                                                   @UserId INT
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    DELETE FROM invitation WHERE user_id = @UserId AND meeting_id = @MeetingId
+END
+go
+
+
