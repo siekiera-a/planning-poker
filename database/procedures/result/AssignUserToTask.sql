30ad0d7ebf6e977cece3da57c36803d1cee8bade
siekiera-a <siekiera.a31@gmail.com>
2021-01-09 02:20:44 +0100

    AssignUserToTask procedure

---
+++ database/procedures/result/AssignUserToTask.sql
@@ -0,0 +1,12 @@
+CREATE PROCEDURE dbo.spResult_AssignUserToTask @UserId INT,
+                                               @TaskId INT,
+                                               @EstimatedTime SMALLINT
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    INSERT INTO result VALUES (@TaskId, @UserId, @EstimatedTime)
+END
+go
+
+
