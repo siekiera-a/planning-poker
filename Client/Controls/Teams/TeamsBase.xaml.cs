16592ecd727a4f648d0f3de7e567e6665f36b87d
Anarak404 <annaarak2@gmail.com>
2021-01-12 02:01:01 +0100

    TeamsView

---
+++ Client/Controls/Teams/TeamsBase.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Client.Controls.Teams
+{
+    /// <summary>
+    /// Interaction logic for TeamsBase.xaml
+    /// </summary>
+    public partial class TeamsBase : UserControl
+    {
+        public TeamsBase()
+        {
+            InitializeComponent();
+        }
+
+        private void AddMemberByEmail(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void GenerateCodeButtonClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
