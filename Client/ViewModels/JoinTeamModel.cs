63f3ba8153c4df4134f455e97cb414e80d780625
Anarak404 <annaarak2@gmail.com>
2021-01-23 00:15:09 +0100

    JoinTeamView connection with viewmodel

--- Client/ViewModels/JoinTeamModel.cs
+++ Client/ViewModels/JoinTeamModel.cs
@@ -1,6 +1,42 @@
-﻿namespace Client.ViewModels
+﻿using System.Windows.Input;
+using Client.Commands;
+using Client.Commands.JoinTeam;
+
+namespace Client.ViewModels
 {
     public class JoinTeamModel : ViewModelBase
     {
+        private string _teamName;
+
+        public string TeamName
+        {
+            get { return _teamName; }
+            set
+            {
+                _teamName = value;
+                OnPropertyChanged(nameof(TeamName));
+            }
+        }
+
+        private string _newTeamName;
+
+        public string NewTeamName
+        {
+            get { return _newTeamName; }
+            set
+            {
+                _newTeamName = value;
+                OnPropertyChanged(nameof(NewTeamName));
+            }
+        }
+
+        public ICommand JoinTeamCommand { get; }
+        public ICommand CreateTeamCommand { get; }
+
+        public JoinTeamModel()
+        {
+            JoinTeamCommand = new JoinTeamCommand(this);
+            CreateTeamCommand = new CreateTeamCommand(this);
+        }
     }
 }
